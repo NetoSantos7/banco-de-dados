1240ff58522d0f3e2d6f2b367e49ef7c3a1ecd49
NetoSantos7 <128547326+NetoSantos7@users.noreply.github.com>
2023-05-02 21:11:39 -0300

    Add files via upload

---
+++ sbcaminhoneiro.sql
@@ -0,0 +1,130 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 03-Maio-2023 às 02:10
+-- Versão do servidor: 10.4.20-MariaDB
+-- versão do PHP: 7.3.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `sbcaminhoneiro`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `administrativo`
+--
+
+CREATE TABLE `administrativo` (
+  `id_Administrativo` int(20) NOT NULL,
+  `salario_Administrativo` double NOT NULL,
+  `cargo_Administrativo` varchar(20) NOT NULL,
+  `id_Funcionario` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `funcionario`
+--
+
+CREATE TABLE `funcionario` (
+  `id_Funcionario` int(20) NOT NULL,
+  `nome_Funcionario` varchar(50) NOT NULL,
+  `cpf_Funcionario` varchar(20) NOT NULL,
+  `endereco_Funcionario` varchar(50) NOT NULL,
+  `telefone_Funcionario` varchar(20) NOT NULL,
+  `dataAD_Funcionario` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `motorista`
+--
+
+CREATE TABLE `motorista` (
+  `id_Motorista` int(20) NOT NULL,
+  `cnh_Motorista` varchar(13) NOT NULL,
+  `salario_Motorista` double NOT NULL,
+  `id_Funcionario` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `administrativo`
+--
+ALTER TABLE `administrativo`
+  ADD PRIMARY KEY (`id_Administrativo`),
+  ADD UNIQUE KEY `id_Funcionario` (`id_Funcionario`);
+
+--
+-- Índices para tabela `funcionario`
+--
+ALTER TABLE `funcionario`
+  ADD PRIMARY KEY (`id_Funcionario`);
+
+--
+-- Índices para tabela `motorista`
+--
+ALTER TABLE `motorista`
+  ADD PRIMARY KEY (`id_Motorista`),
+  ADD KEY `id_Funcionario` (`id_Funcionario`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `administrativo`
+--
+ALTER TABLE `administrativo`
+  MODIFY `id_Administrativo` int(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `funcionario`
+--
+ALTER TABLE `funcionario`
+  MODIFY `id_Funcionario` int(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `motorista`
+--
+ALTER TABLE `motorista`
+  MODIFY `id_Motorista` int(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `funcionario`
+--
+ALTER TABLE `funcionario`
+  ADD CONSTRAINT `funcionario_ibfk_1` FOREIGN KEY (`id_Funcionario`) REFERENCES `administrativo` (`id_Funcionario`);
+
+--
+-- Limitadores para a tabela `motorista`
+--
+ALTER TABLE `motorista`
+  ADD CONSTRAINT `motorista_ibfk_1` FOREIGN KEY (`id_Funcionario`) REFERENCES `administrativo` (`id_Funcionario`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
